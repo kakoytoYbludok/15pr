807e7ff01cadd1100ff5d9b5162385b596e64d63
Markus <isip_m.e.letkeman@mpt.ru>
2024-04-03 16:21:31 +0300

    pipline

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!asdfasfdfsdasfafsasfaadsfdsfsadfsadf sapdfj ;lksadj f;laksjd ;lfkjdsa;l kfja;lskdj f;alsdk jf;lsakdj f;laksd jf;lkasj df;lksaj df;lkajsd;l kjsaf; lkfja;l sdkjf ;lsdaj ;lkfjsa;lk djf;lksaj'
-
+    return 'Hello World!asdfa'
 app.run()
